d256e7533065c4389018b12bd9510329a4227e6b
Patrick Niemann <niemann.patrick@gmail.com>
2022-03-16 11:43:55 +0100

    Added build script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "Version: $1"
+set -e
 docker build -t nextcloud-pi .
 docker login -u panic158
 docker tag nextcloud-pi:latest panic158/nextcloud-pi:latest
+docker tag nextcloud-pi:latest panic158/nextcloud-pi:$1
 docker push panic158/nextcloud-pi:latest 
+docker push panic158/nextcloud-pi:$1 
