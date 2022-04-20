99344e269a011dbe755f2996e2c0ab24b7a86f67
Patrick Niemann <niemann.patrick@gmail.com>
2022-04-20 11:09:50 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "Version: $1"
 set -e
-docker build -t nextcloud-pi .
+docker build --platform linux/arm/v7 -t nextcloud-pi .
 docker login -u panic158
 docker tag nextcloud-pi:latest panic158/nextcloud-pi:latest
 docker tag nextcloud-pi:latest panic158/nextcloud-pi:$1
